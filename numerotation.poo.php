c9a8c62e8712fdb6809d003dd4c396798a2b4b9d
philbotche <votredeveloppeur@gmail.com>
2021-08-25 22:34:55 +0200

    Suppression de methode dans la numerotation.poo.php

--- numerotation.poo.php
+++ numerotation.poo.php
@@ -9,18 +9,4 @@ class Numero {
         $this->num = $num;
     }
 
-    //pour générer un input de type number
-    public function input ($name) {
-        return '<input type="number" class="form-control" id="inputPassword2" name="' . $name . '" placeholder="Entrer le numero">';
-    }
-    
-    //pour générer un bouton de type submit
-    public function submit () {
-        return '<button type="submit" class="btn btn-primary mb-3">Valider</button>';
-    }
-
-    private function counter ($taille) {
-        $this->count($taille);
-    }
-
 }
